8d3d5f5335918970c0e9d9651fd5df92b5705cb4
pedegie <kacper-ciepluch@o2.pl>
2022-02-06 21:40:44 +0100

    84 JMH benchmark for ProbeTailerImpl

--- jmh/src/main/java/io/github/pedegie/stats/jmh/ProbeTailerJMH.java
+++ jmh/src/main/java/io/github/pedegie/stats/jmh/ProbeTailerJMH.java
@@ -16,6 +16,7 @@ import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -34,6 +35,25 @@ import java.util.stream.IntStream;
 
 import static io.github.pedegie.stats.jmh.BenchmarkUtils.randomPath;
 
+/*
+Benchmark                                           (batchSize)   Mode  Cnt   Score   Error  Units
+ProbeTailerJMH.TestBenchmark5.ProbeTailerBenchmark            1  thrpt    4   1.505 ± 0.073  ops/s
+ProbeTailerJMH.TestBenchmark5.ProbeTailerBenchmark           25  thrpt    4  10.159 ± 0.944  ops/s
+ProbeTailerJMH.TestBenchmark5.ProbeTailerBenchmark           50  thrpt    4  11.494 ± 0.889  ops/s
+ProbeTailerJMH.TestBenchmark5.ProbeTailerBenchmark          100  thrpt    4  12.156 ± 1.498  ops/s
+ProbeTailerJMH.TestBenchmark5.ProbeTailerBenchmark         1000  thrpt    4  11.935 ± 0.857  ops/s
+ProbeTailerJMH.TestBenchmark5.ProbeTailerBenchmark        10000  thrpt    4  12.416 ± 0.393  ops/s
+
+Relative cost of thereIsSomethingToRead() to readProbesFromBatchBytes() decreases while increasing batch size
+batchSize | relative cost of thereIsSomethingToRead()
+1         | 85%
+25        | 28%
+50        | 18%
+100       | 15%
+1000      | 10%
+10000     | 10%
+*/
+
 public class ProbeTailerJMH
 {
     private static final int PROBES = 10_000_000;
@@ -41,8 +61,8 @@ public class ProbeTailerJMH
     @Fork(value = 1)
     @Warmup(iterations = 5)
     @Measurement(iterations = 4)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @BenchmarkMode({Mode.AverageTime})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @BenchmarkMode({Mode.Throughput})
     @State(Scope.Benchmark)
     @Timeout(time = 120)
     public static class TestBenchmark5
@@ -50,29 +70,29 @@ public class ProbeTailerJMH
         @Benchmark
         public void ProbeTailerBenchmark(QueueConfiguration5 queueConfiguration)
         {
-            int readSize = 50;
-            for (int i = 0; i < PROBES; i += readSize)
-                queueConfiguration.probeTailer.read(readSize);
+            queueConfiguration.probeTailer.readFromStart();
         }
     }
 
     @State(Scope.Benchmark)
     public static class QueueConfiguration5
     {
+        @Param({"1", "25", "50", "100", "1000", "10000"})
+        public int batchSize;
+
         StatsQueue<Integer> statsQueue;
         ProbeTailer probeTailer;
 
         @Setup(Level.Iteration)
         public void setUp()
         {
-
             FileUtils.cleanDirectory(BenchmarkUtils.testQueuePath);
             Path path = randomPath();
             var queueConfiguration = QueueConfiguration.builder()
                     .path(path)
                     .preTouch(true)
                     .mmapSize(4L << 30)
-                    .batching(new Batching(100, Long.MAX_VALUE))
+                    .batching(new Batching(batchSize, Long.MAX_VALUE))
                     .disableSynchronization(true)
                     .build();
 
@@ -110,6 +130,7 @@ public class ProbeTailerJMH
         @Override
         public void onProbe(Probe probe)
         {
+
         }
     }
 }
