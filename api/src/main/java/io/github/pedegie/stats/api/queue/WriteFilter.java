1c88b9f34d936bdf3b36ccbd3a6f11db3b9c2be2
pedegie <kacper-ciepluch@o2.pl>
2022-02-06 11:19:44 +0100

    83-WriteFilter acceptWhenSizeHigherThan lightweight pattern implementation

--- api/src/main/java/io/github/pedegie/stats/api/queue/WriteFilter.java
+++ api/src/main/java/io/github/pedegie/stats/api/queue/WriteFilter.java
@@ -3,45 +3,15 @@ package io.github.pedegie.stats.api.queue;
 @FunctionalInterface
 public interface WriteFilter
 {
-    WriteFilter acceptAllFilter = acceptAll();
-
     boolean shouldWrite(int size, long timestamp);
 
-    private static WriteFilter acceptAll()
+    static WriteFilter acceptAll()
     {
-        return new WriteFilter()
-        {
-            @Override
-            public boolean shouldWrite(int size, long timestamp)
-            {
-                return true;
-            }
-
-            @Override
-            public String toString()
-            {
-                return "acceptAllFilter";
-            }
-        };
+        return WriteFilterFactory.acceptAllFilter();
     }
 
     static WriteFilter acceptWhenSizeHigherThan(int size)
     {
-        return new WriteFilter()
-        {
-            private final int acceptSize = size;
-
-            @Override
-            public boolean shouldWrite(int size, long timestamp)
-            {
-                return size > acceptSize;
-            }
-
-            @Override
-            public String toString()
-            {
-                return "acceptWhenSizeHigherThan " + acceptSize;
-            }
-        };
+        return WriteFilterFactory.acceptWhenSizeHigherThan(size);
     }
 }
